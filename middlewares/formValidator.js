fcf8ece5c1c122f598e8a333a4167e48aeae220a
zuhair khan <zuhairkhan5134@gmail.com>
2025-04-20 14:34:06 +0530

    bug fix

--- middlewares/formValidator.js
+++ middlewares/formValidator.js
@@ -4,6 +4,6 @@ export const formValidationRules = [
   body('firstName').notEmpty().withMessage('First name is required'),
   body('lastName').notEmpty().withMessage('Last name is required'),
   body('emailId').isEmail().withMessage('Valid email is required'),
-  body('mark').isBoolean().withMessage('Mark must be a boolean'),
+  // body('mark').isBoolean().withMessage('Mark must be a boolean'),
   body('message').notEmpty().withMessage('Message is required'),
 ];
