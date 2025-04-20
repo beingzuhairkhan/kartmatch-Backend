ebdd15fe879fdf7b8d16be7e5975ad0c0286b0c8
zuhair khan <zuhairkhan5134@gmail.com>
2025-04-20 14:45:10 +0530

    cors error

--- index.js
+++ index.js
@@ -10,7 +10,24 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+    'https://kart-match.vercel.app',
+    'http://localhost:3000'
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true // if you want to allow cookies and credentials
+  }));
+  
 app.use(express.json());
 
 // MongoDB connection
